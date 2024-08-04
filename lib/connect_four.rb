e1f68c343b790d6576265eb355db2e6f4c21f272
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-08-04 14:40:34 +0700

    Add check_vertical and four_items_in_row? method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -23,6 +23,34 @@ class Grid
     column.length >= 6
   end
 
+  def player_win?
+    pass
+  end
+
+  def check_vertical
+    @grid.each do |column|
+      x_sum = column.count('x')
+      o_sum = column.count('o')
+      next unless x_sum >= 4 || o_sum >= 4
+
+      return true if four_items_in_row?(column)
+    end
+  end
+
+  def four_items_in_row?(column)
+    temp_column = []
+
+    column.each do |item|
+      return true if temp_column.length >= 4
+
+      if item == choose_player
+        temp_column << item
+      else
+        temp_column = []
+      end
+    end
+  end
+
   def choose_player
     player = %w[x o]
     player[@turn % 2]
